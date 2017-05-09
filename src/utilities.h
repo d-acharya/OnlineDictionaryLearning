040a4545d99eb6f85be7854912e62a82d05c2ef4
lunaivory <catlily234@gmail.com>
2017-05-09 10:10:07 +0200

    remove utilities

--- src/utilities.h
+++
@@ -1,6 +0,0 @@
-#include "mathOperations.h"
-#include "mkl.h"
-
-double larsCost(double * D, double * x, double * a, int cols, int rows, double lambda);
-double updateCost(D, A, B, rows, cols);
-void updateDict(D, A, B, int rows, int cols);
